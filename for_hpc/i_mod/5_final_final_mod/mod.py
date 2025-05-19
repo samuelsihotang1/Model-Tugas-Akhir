e7caa131835c9438046d5604ba057c77e62ad7b3
Samuel Sihotang <samsihotang1@gmail.com>
2025-05-19 16:38:13 +0700

    `

--- for_hpc/i_mod/5_final_final_mod/mod.py
+++ for_hpc/i_mod/5_final_final_mod/mod.py
@@ -719,9 +719,9 @@ from timm.utils import ApexScaler, NativeScaler
 def _assign_hyperparameter(args):
     ### CUSTOM ###
     # Load this checkpoint as if they were the pretrained weights (with adaptation) (default: None).
-    args.pretrained_path = None
+    args.pretrained_path = '/home/tasi2425111/for_hpc/baru/i_mod/5_final_final_mod/output/train/20250517-182114-mobile_former_294m-224/checkpoint-6.pth.tar'
     # Resume full model and optimizer state from checkpoint (default: '')
-    args.resume = ''
+    args.resume = '/home/tasi2425111/for_hpc/baru/i_mod/5_final_final_mod/output/train/20250517-182114-mobile_former_294m-224/checkpoint-6.pth.tar'
     # path to dataset (root dir)
     args.data_dir = '/home/tasi2425111/restructured_resized_imagenet'  #Disesuaikan dengan kebutuhan
     # number of label classes (Model default if None)
@@ -729,7 +729,7 @@ def _assign_hyperparameter(args):
     # Name of model to train (default: "resnet50")
     args.model = 'mobile_former_294m' #mobile_former_294m  #Disesuaikan dengan kebutuhan
     # Device (accelerator) to use.
-    args.device = 'cuda:1'
+    args.device = 'cuda:0'
     args.patience_epochs = 10
 
     # Input image center crop percent (for validation only)
